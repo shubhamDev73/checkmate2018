d28b23330d986ad280d4922c1df0855c76d3ad0d
Shubham Gupta <shubham0209@gmail.com>
2018-08-24 17:35:26 +0530

    Added walls to minigame2

--- Assets/_Scripts/MazePlate.cs
+++ Assets/_Scripts/MazePlate.cs
@@ -2,26 +2,36 @@ using UnityEngine;
 
 public class MazePlate : MonoBehaviour {
 
+	public GameObject wallPrefab;
+
 	private static float cost = 0, lastCost = 0;
 	private static Vector3 lastPlateLocation;
 	private static bool updateXMode = false, updateYMode = false; // Considers whether to update only in y or x
 
 	void OnTriggerEnter(Collider col) {
 		if(col.CompareTag("Player")){
-			//Make Wall
+			int rot = 180;
+			col.gameObject.transform.position = new Vector3(transform.position.x, col.gameObject.transform.position.y, transform.position.z);
 			if(updateXMode)
-				if(Vector3.Dot((transform.position - lastPlateLocation).normalized, transform.right) == 1)
+				if(Vector3.Dot((transform.position - lastPlateLocation).normalized, transform.right) == 1){
 					cost = lastCost - 2;
-				else
+					rot = 270;
+				}else{
 					cost = lastCost + 2;
+					rot = 90;
+				}
 			
 			if(updateYMode)
-				if(Vector3.Dot((transform.position - lastPlateLocation).normalized, transform.forward) == 1)
+				if(Vector3.Dot((transform.position - lastPlateLocation).normalized, transform.forward) == 1){
 					cost = lastCost * 2;
-				else
+					rot = 180;
+				}else{
 					cost = lastCost / 2;
+					rot = 0;
+				}
 			
 			Debug.Log("x: " + updateXMode.ToString() + ", y: " + updateYMode.ToString() + ", cost: " + cost.ToString());
+			Instantiate(wallPrefab, transform.position, Quaternion.Euler(0, rot, 0));
 
 			updateXMode = false;
 			updateYMode = false;
@@ -30,16 +40,18 @@ public class MazePlate : MonoBehaviour {
 
 	void OnTriggerExit (Collider col) {
 		if(col.CompareTag("Player")){
+			int rot = 180;
 			if(Mathf.Abs(Vector3.Dot((col.transform.position - transform.position).normalized, transform.right)) > Mathf.Abs(Vector3.Dot((col.transform.position - transform.position).normalized, transform.forward))){
 				updateXMode = true;
 				updateYMode = false;
+				rot = 180 - ((int)Mathf.Sign(Vector3.Dot((col.transform.position - transform.position).normalized, transform.right)) * 90);
 			}else{
 				updateXMode = false;
 				updateYMode = true;
 			}
 			lastPlateLocation = transform.position;
 			lastCost = cost;
-			// Make Wall
+			Instantiate(wallPrefab, transform.position, Quaternion.Euler(0, rot, 0));
 		}
 	}
 	

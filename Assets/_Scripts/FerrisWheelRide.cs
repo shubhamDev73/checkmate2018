1e448851954d89242f246c8907ceb7c48802adfb
coolsidd <sidharth_0903@hotmail.com>
2018-09-02 13:12:54 +0530

    Implemented basic rides framework

---
+++ Assets/_Scripts/FerrisWheelRide.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class FerrisWheelRide : MonoBehaviour {
+
+    public GameObject []seats;
+    public RideToggle script;
+    public Transform player;
+    public float rotationSpeed;
+    private int mainSeat;
+    private bool _rideStart;
+    private bool rideStart
+    {
+        set{
+            _rideStart = value;
+            if(value)
+            {
+                mainSeat = LowestSeat();
+            }
+        }
+        get{return _rideStart;}
+    }
+	void Start () {
+		_rideStart = false;
+	}
+
+    int LowestSeat()
+    {
+        float lowest = seats[0].transform.position.y;
+        int min = 0;
+        for(int i=1;i<seats.Length;i++)
+        {
+            if(seats[i].transform.position.y < lowest)
+                min = i;
+        }
+        return min;
+    }
+
+	void Update () {
+        if(rideStart && script.onRide)
+        {
+            player.position = seats[mainSeat].transform.position - new Vector3(0, 1.5f, 0);
+            transform.Rotate(0, 0, rotationSpeed, Space.Self);
+            return;
+        }
+        else if(script.onRide)
+        {
+            rideStart = true;
+        }
+        else
+        {
+            rideStart = false;
+        }
+
+
+	}
+}
---
+++ Assets/_Scripts/RideToggle.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RideToggle : MonoBehaviour {
+    public Transform exitLocation, player;
+    private bool _onRide;
+    private float playerLastY;
+    public bool onRide
+    {
+        get {return _onRide;}
+        set
+        {
+            _onRide = value;
+            if(_onRide)
+            {
+                playerLastY = player.position.y;
+            }
+            else
+            {
+                player.position = new Vector3(exitLocation.position.x,playerLastY,exitLocation.position.z);
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        Debug.Log("Entered");
+        if(col.CompareTag("Player") && Input.GetButtonDown("Click"))
+        {
+            Debug.Log("Starting Ride");
+            onRide = true;
+        }
+    }
+	void Start () {
+        _onRide = false;
+
+	}
+
+
+	void Update () {
+        if(onRide)
+        {
+            if(Input.GetButtonDown("Exit"))
+            {
+                Debug.Log("Exiting");
+                onRide = false;
+                return;
+            }
+        }
+	}
+}
